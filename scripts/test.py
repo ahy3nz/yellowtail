f605586a4783ea1bd29a593b813d3c3320e9b6bb
Alex Yang <ayang41@gmail.com>
2021-08-29 21:16:52 -0400

    Adjust file paths

--- scripts/test.py
+++ scripts/test.py
@@ -1,7 +1,8 @@
 import numpy
 import datetime
+from pathlib import Path
 
 print("HELLOWORLD")
 print(datetime.datetime.now())
-with open("../output/test.txt", 'w') as f:
+with open("output/test.txt", 'w') as f:
     f.write(str(datetime.datetime.now()))
